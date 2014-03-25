089714a668a0251c31fe3ee0dcd8c92c166b1d62
Michael Phillips <michael@crushlovely.com>
2014-03-25 14:54:08 -0400

    Tests for service object

---
+++ test/02_service.js
@@ -0,0 +1,19 @@
+var UserAuthenticator = require('../examples/02_service');
+var expect = require('chai').expect;
+
+
+
+var user;
+
+describe('UserAuthenticator', function() {
+
+  beforeEach( function() {
+    user = { unencryptedPassword: 'f00b4r' };
+  })
+
+  it('#authenticate', function() {
+    expect( new UserAuthenticator( user ).authenticate('foobar') ).to.equal( false );
+    expect( new UserAuthenticator( user ).authenticate('f00b4r') ).to.equal( user );
+  })
+
+})
