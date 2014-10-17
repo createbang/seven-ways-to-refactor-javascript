eaccde3350911da931ce0804c4a96b132f4cfd5e
Eduardo Cancino <eduardo.cancino@globant.com>
2014-10-17 15:27:05 -0500

    Service objects, added class and test from post
    Class: https://gist.github.com/createbang/1a2bcc38c6e3cc603ccb#file-determinestudentpassingstatus-js
    Test: https://gist.github.com/createbang/021c975547f63c5daed6#file-determinestudentpassingstatus-test-js

--- test/02_service.js
+++ test/02_service.js
@@ -1,19 +1,65 @@
-var UserAuthenticator = require('../examples/02_service');
 var expect = require('chai').expect;
+var DetermineStudentPassingStatus = require('../examples/02_service');
+var Grade = require('../examples/01_value');
 
+describe('DetermineStudentPassingStatus', function(){
+  var student = {};
+  var assignments = [
+    {grade: new Grade(0.5)},
+    {grade: new Grade(0.8)},
+    {grade: new Grade(0.9)},
+    {grade: new Grade(0.6)},
+  ];
+  var determineStudentPassingStatus = new DetermineStudentPassingStatus( student );
 
+  describe('#extractAssignmentGrades', function(){
 
-var user;
+    it('returns an array of grade value objects', function(){
+      var grades = determineStudentPassingStatus.extractAssignmentGrades( assignments );
+      expect( grades[0] ).to.be.an.instanceof( Grade );
+    });
 
-describe('Service', function() {
+  });
 
-  beforeEach( function() {
-    user = { unencryptedPassword: 'f00b4r' };
-  })
+  describe('#averageAssignmentGrade', function(){
 
-  it('#authenticate', function() {
-    expect( new UserAuthenticator( user ).authenticate('foobar') ).to.equal( false );
-    expect( new UserAuthenticator( user ).authenticate('f00b4r') ).to.equal( user );
-  })
+    it('returns the average of all of the grades', function(){
+      var grades = determineStudentPassingStatus.extractAssignmentGrades( assignments );
+      var averageGrade = determineStudentPassingStatus.averageAssignmentGrade( grades );
+      expect( averageGrade ).to.equal( ( (0.5 + 0.8 + 0.9 + 0.6) / 4 ) );
+    });
 
-})
+  });
+
+  describe('#determinePassingStatus', function(){
+
+    it('returns whether or not the student is passing', function(){
+      var grades = determineStudentPassingStatus.extractAssignmentGrades( assignments );
+      var averageGrade = determineStudentPassingStatus.averageAssignmentGrade( grades );
+      var passing = determineStudentPassingStatus.determinePassingStatus( averageGrade );
+      expect( passing ).to.be.true;
+    });
+
+  });
+
+  describe('#fromAssignments', function(){
+    var passing;
+
+    it('returns the correct passing state', function(){
+      passing = determineStudentPassingStatus.fromAssignments( assignments );
+      expect( passing ).to.be.true;
+
+      // overwrite to test false return
+      assignments = [
+        {grade: new Grade(0.5)},
+        {grade: new Grade(0.4)},
+        {grade: new Grade(0.8)},
+        {grade: new Grade(0.6)},
+      ];
+      passing = determineStudentPassingStatus.fromAssignments( assignments );
+      expect( passing ).to.be.false;
+    });
+
+  });
+
+});
